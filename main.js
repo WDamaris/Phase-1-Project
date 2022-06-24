3df5a9aac1770ae4af80ccfc229c15c623a52722
WDamaris <damarisshish@gmail.com>
2022-06-24 12:58:00 +0300

    written a function to fetch movies from url

--- main.js
+++ main.js
@@ -87,6 +87,18 @@
 const API_KEY='api_key=9f7a2b3abcaf511ee12205295aa3d257';
 const BASE_URL='https://api.themoviedb.org/3';
 const API_URL= BASE_URL+'/discover/movie?sort_by=popularity.desc&'+API_KEY;
+getMovies(API_URL);
+function getMovies(url){
+
+    fetch(url)
+    .then(res =>res.json())
+    .then(data =>{
+        showMovies(data.results);
+    })
+
+}
+
+
 
 
 
