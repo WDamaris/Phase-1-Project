2b788166e0fead328122e4c14d35014ee1afedac
WDamaris <damarisshish@gmail.com>
2022-06-24 13:28:08 +0300

    copied image url from The Movie Database to my javaScript

--- main.js
+++ main.js
@@ -87,6 +87,7 @@
 const API_KEY='api_key=9f7a2b3abcaf511ee12205295aa3d257';
 const BASE_URL='https://api.themoviedb.org/3';
 const API_URL= BASE_URL+'/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL='https://image.tmdb.org/t/p/w500'
 getMovies(API_URL);
 function getMovies(url){
 
